4518120a6521d7d54ee8ee7e7f0386fbe4b8bdb2
AryoBama <13523088@std.stei.itb.ac.id>
2025-05-18 14:35:28 +0700

    feat: Add method for count visited node

--- src/backend/src/main/java/com/project/backend/controller/SolveController.java
+++ src/backend/src/main/java/com/project/backend/controller/SolveController.java
@@ -3,9 +3,11 @@ package com.project.backend.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,14 +21,22 @@ import com.project.backend.models.BoardRequest;
 @RestController
 @RequestMapping("/api/solve")
 public class SolveController {
-
+    @CrossOrigin(origins = "http://localhost:3000") 
     @PostMapping("/UCS")
     public ResponseEntity<Map<String, Object>>SolveWithUCS(@RequestBody BoardRequest request) {
         Map<String, Object> response = new HashMap<>();
         List<Map<String, Object>> steps = new ArrayList<>();
 
+        
         Board board = request.convertToBoard();
-        List<BoardNode> result = UCS.solveUCS(board);
+
+        long startTime = System.nanoTime();
+
+        List<BoardNode> result = new LinkedList<>();
+        Integer cntNode = UCS.solveUCS(board, result);
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        duration /= 1_000_000.0;
 
         for(BoardNode node : result){
             Map<String, Object> step = new HashMap<>();
@@ -35,8 +45,11 @@ public class SolveController {
             step.put("direction", node.getState().getDirections());
             steps.add(step);
         }
+        board.displayBoard();
 
         response.put("steps", steps);
+        response.put("time", duration);
+        response.put("cntNode", cntNode);
         return ResponseEntity.ok(response);
     }
 }
