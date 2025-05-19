4a9aeab0190b5ee0d1296f48a9e2fb6e4a88b820
AryoBama <13523088@std.stei.itb.ac.id>
2025-05-19 16:59:37 +0700

    feat: Add new endpoint

--- src/backend/src/main/java/com/project/backend/controller/SolveController.java
+++ src/backend/src/main/java/com/project/backend/controller/SolveController.java
@@ -2,8 +2,8 @@ package com.project.backend.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
@@ -13,10 +13,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.backend.algorithms.AStar.AStar;
+import com.project.backend.algorithms.IDA.IDA;
 import com.project.backend.algorithms.UCS.UCS;
+import com.project.backend.heuristic.BlockingCar;
+import com.project.backend.heuristic.BlockingChain;
+import com.project.backend.heuristic.CountHeuristic;
 import com.project.backend.models.Board;
-import com.project.backend.models.BoardNode;
 import com.project.backend.models.BoardRequest;
+import com.project.backend.models.BoardState;
 
 @RestController
 @RequestMapping("/api/solve")
@@ -30,19 +35,87 @@ public class SolveController {
         
         Board board = request.convertToBoard();
 
+        
+        List<BoardState> result = new LinkedList<>();
         long startTime = System.nanoTime();
-
-        List<BoardNode> result = new LinkedList<>();
         Integer cntNode = UCS.solveUCS(board, result);
         long endTime = System.nanoTime();
         long duration = endTime - startTime;
         duration /= 1_000_000.0;
+        System.out.println("Durasinya: " + duration);
 
-        for(BoardNode node : result){
+        for(BoardState node : result){
             Map<String, Object> step = new HashMap<>();
-            step.put("board", node.getState().getGrid());
-            step.put("piece", node.getState().getCar().getId());
-            step.put("direction", node.getState().getDirections());
+            step.put("board", node.getGrid());
+            step.put("piece", node.getCar().getId());
+            step.put("direction", node.getDirections());
+            steps.add(step);
+        }
+        board.displayBoard();
+
+        response.put("steps", steps);
+        response.put("time", duration);
+        response.put("cntNode", cntNode);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/IDA")
+    public ResponseEntity<Map<String, Object>>SolveWithIDA(@RequestBody BoardRequest request) {
+        Map<String, Object> response = new HashMap<>();
+        List<Map<String, Object>> steps = new ArrayList<>();
+
+        
+        Board board = request.convertToBoard();
+
+        CountHeuristic heuristic = new BlockingCar(board);
+        
+        List<BoardState> result = new LinkedList<>();
+        long startTime = System.nanoTime();
+        IDA ida = new IDA();
+        Integer cntNode = ida.solveIDA(board, heuristic, result);
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        duration /= 1_000_000.0;
+        System.out.println("Durasinya: " + duration);
+
+        for(BoardState node : result){
+            Map<String, Object> step = new HashMap<>();
+            step.put("board", node.getGrid());
+            step.put("piece", node.getCar().getId());
+            step.put("direction", node.getDirections());
+            steps.add(step);
+        }
+        board.displayBoard();
+
+        response.put("steps", steps);
+        response.put("time", duration);
+        response.put("cntNode", cntNode);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/AStar")
+    public ResponseEntity<Map<String, Object>>SolveWithAStar(@RequestBody BoardRequest request) {
+        Map<String, Object> response = new HashMap<>();
+        List<Map<String, Object>> steps = new ArrayList<>();
+
+        
+        Board board = request.convertToBoard();
+
+        CountHeuristic heuristic = new BlockingCar(board);
+        
+        List<BoardState> result = new LinkedList<>();
+        long startTime = System.nanoTime();
+        Integer cntNode = AStar.solveAStar(board, heuristic,result);
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        duration /= 1_000_000.0;
+        System.out.println("Durasinya: " + duration);
+
+        for(BoardState node : result){
+            Map<String, Object> step = new HashMap<>();
+            step.put("board", node.getGrid());
+            step.put("piece", node.getCar().getId());
+            step.put("direction", node.getDirections());
             steps.add(step);
         }
         board.displayBoard();
