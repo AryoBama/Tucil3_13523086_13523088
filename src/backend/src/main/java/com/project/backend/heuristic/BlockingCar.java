1d5a4810437b729c5edaf64039924490b4c51140
AryoBama <13523088@std.stei.itb.ac.id>
2025-05-19 16:58:59 +0700

    fix: Change parameter in function

--- src/backend/src/main/java/com/project/backend/heuristic/BlockingCar.java
+++ src/backend/src/main/java/com/project/backend/heuristic/BlockingCar.java
@@ -1,51 +1,18 @@
 package com.project.backend.heuristic;
 
-import com.project.backend.models.*;
+import com.project.backend.models.Board;
 
 public class BlockingCar implements CountHeuristic {
 
+    private Board b;
+
+    public BlockingCar(Board b){
+        this.b = b;
+    }
+
     @Override
     public int getValue(Board b){
-
-        int countCar = 0;
-
-        Car mainCar = b.getCarById('P');
-
-        int mainRow = mainCar.getStartRow();
-        int mainCol = mainCar.getStartCol();
-        int mainLength = mainCar.getLength();
-
-
-        if (mainCar.getOrientation().equals("horizontal")){
-
-            if (mainCol < b.getExitCol()){
-                for (int i = mainLength + mainCol; i < b.getWidth(); i++){
-                    if (!b.isSpaceEmpty(mainRow, i)){
-                        countCar++;
-                    }
-                }
-            }else{
-                for (int i = mainCol - 1; i >= 0; i--){
-                    if (!b.isSpaceEmpty(mainRow, i)){
-                        countCar++;
-                    }
-                }             
-            }
-        }else{
-            if (mainRow < b.getExitRow()){
-                for (int i = mainLength + mainRow; i < b.getHeight(); i++){
-                    if (!b.isSpaceEmpty(i, mainCol)){
-                        countCar++;
-                    }
-                }
-            }else{
-                for (int i = mainRow + 1; i >= 0; i--){
-                    if (!b.isSpaceEmpty(i, mainCol)){
-                        countCar++;
-                    }
-                }             
-            }        
-        }
-        return countCar;
+        this.b = b;
+        return b.getBlockingCar().size();
     }
 }
